776d3be91702560ab9921d7c62dfe7c20c75375a
Johan Dahlberg <johan.dahlberg@medsci.uu.se>
2016-03-12 14:37:51 +0100

    Initial work on bar plot

--- curiouscrows/app.py
+++ curiouscrows/app.py
@@ -8,6 +8,7 @@ from flask.ext.bootstrap import Bootstrap
 from bokeh.plotting import figure, ColumnDataSource
 from bokeh.embed import components
 from bokeh.models import (HoverTool, BoxZoomTool, ResetTool, TapTool, CustomJS)
+from bokeh.charts import Bar
 
 from sklearn.preprocessing import Imputer, scale
 from sklearn.decomposition import PCA
@@ -39,12 +40,12 @@ def compute_principal_components():
     imp = Imputer(strategy="mean", axis=1)
     imp.fit(mx)
     # Impute mean values
-    df = pd.DataFrame(imp.transform(mx), index=mx.index)
+    df = pd.DataFrame(imp.transform(mx), index=mx.index, columns=mx.columns.values)
     colvar = df.var(axis=0)
     # Remove constant columns
     df2 = df[colvar[colvar > 0.01].index]
     # Scale values
-    df3 = pd.DataFrame(scale(df2), index=mx.index)
+    df3 = pd.DataFrame(scale(df2), index=mx.index, columns=df2.columns.values)
 
     pca = PCA(n_components=2)
     pc = pca.fit(df3).transform(df3)
@@ -52,11 +53,7 @@ def compute_principal_components():
     return df3, pc
 
 
-df3, pc = compute_principal_components()
-
-
-@app.route('/')
-def index():
+def create_pca_plot():
     """Render the initial PCA plot with all regions."""
     callback = CustomJS(code="""
         var index = cb_obj.get('selected')['1d'].indices[0];
@@ -82,17 +79,56 @@ def index():
         )
     )
 
-    p = figure(title='PCA plot', plot_width=960, plot_height=600, tools=tools,
-               responsive=True)
+    pca_figure = figure(title='PCA plot', plot_width=960, plot_height=600, tools=tools,
+                        responsive=True)
 
-    p.circle('x', 'y', size=10, fill_color='navy', source=source, alpha=0.5,
-             hover_fill_color="firebrick")
+    pca_figure.circle('x', 'y', size=10, fill_color='navy', source=source, alpha=0.5,
+                      hover_fill_color="firebrick")
 
-    p.xaxis.axis_label = "x"
-    p.yaxis.axis_label = "y"
+    pca_figure.xaxis.axis_label = "x"
+    pca_figure.yaxis.axis_label = "y"
 
-    fig_js, fig_div = components(p)
-    return render_template("figures.html", figJS=fig_js, figDiv=fig_div)
+    pca_fig_js, pca_fig_div = components(pca_figure)
+    return pca_fig_js, pca_fig_div
+
+def create_bar_plot():
+
+    small_data = df3.head()
+    small_data["municipality_name"] = small_data.index
+
+    small_data_melted = pd.melt(small_data, id_vars='municipality_name')
+
+    small_data_melted = small_data_melted[
+        (small_data_melted.variable == "N00002") |
+        (small_data_melted.variable == "N00003") |
+        (small_data_melted.variable == "N00005  ")
+    ]
+
+    bar_plot = Bar(
+        small_data_melted,
+        'municipality_name',
+        values='value',
+        group='variable',
+        title="Test bar plot")
+
+    bar_fig_js, bar_fig_div = components(bar_plot)
+    return bar_fig_js, bar_fig_div
+
+
+df3, pc = compute_principal_components()
+
+@app.route('/')
+def index():
+    pca_fig_js, pca_fig_div = create_pca_plot()
+    bar_fig_js, bar_fig_div = create_bar_plot()
+
+    return \
+        render_template(
+            "figures.html",
+            pcaFigJs=pca_fig_js,
+            pcaFigDiv=pca_fig_div,
+            barFigJs=bar_fig_js,
+            barFigDiv=bar_fig_div)
 
 
 def start():
