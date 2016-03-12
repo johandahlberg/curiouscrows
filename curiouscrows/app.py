9c395de4d82c41dd862eb72747a7e93055930edf
Robin Andeer <robin.andeer@gmail.com>
2016-03-12 14:11:40 +0100

    add list of selected kommuner

--- curiouscrows/app.py
+++ curiouscrows/app.py
@@ -7,7 +7,8 @@ from flask.ext.bootstrap import Bootstrap
 
 from bokeh.plotting import figure, ColumnDataSource
 from bokeh.embed import components
-from bokeh.models import (HoverTool, BoxZoomTool, ResetTool, TapTool, CustomJS)
+from bokeh.models import (HoverTool, BoxZoomTool, ResetTool, TapTool, CustomJS,
+                          BoxSelectTool)
 
 from sklearn.preprocessing import Imputer, scale
 from sklearn.decomposition import PCA
@@ -59,11 +60,12 @@ df3, pc = compute_principal_components()
 def index():
     """Render the initial PCA plot with all regions."""
     callback = CustomJS(code="""
-        var index = cb_obj.get('selected')['1d'].indices[0];
-        if (index !== undefined) {
-            var desc = cb_obj.get('data').desc[index];
-            history.pushState({}, '', '#' + desc);
+        var indexes = cb_obj.get('selected')['1d'].indices;
+        var descs = [];
+        for (index in indexes) {
+            descs.push(cb_obj.get('data').desc[indexes[index]]);
         }
+        window.vue.$data.regions = descs;
     """)
 
     tools = [HoverTool(
@@ -72,7 +74,7 @@ def index():
             ("(x,y)", "($x, $y)"),
             ("desc", "@desc"),
         ]
-    ), BoxZoomTool(), ResetTool(), TapTool(callback=callback)]
+    ), BoxZoomTool(), ResetTool(), TapTool(callback=callback), BoxSelectTool(callback=callback)]
 
     source = ColumnDataSource(
         data=dict(
@@ -82,16 +84,25 @@ def index():
         )
     )
 
-    p = figure(title='PCA plot', plot_width=960, plot_height=600, tools=tools,
-               responsive=True)
+    plot = figure(title='PCA plot', plot_width=960, plot_height=600,
+                  tools=tools, responsive=True)
+    plot.background_fill_color = 'beige'
+    plot.background_fill_alpha = 0.5
 
-    p.circle('x', 'y', size=10, fill_color='navy', source=source, alpha=0.5,
-             hover_fill_color="firebrick")
+    renderer = plot.circle('x', 'y', size=10, source=source,
+                           # set visual properties for selected glyphs
+                           selection_color="firebrick",
 
-    p.xaxis.axis_label = "x"
-    p.yaxis.axis_label = "y"
+                           # set visual properties for non-selected glyphs
+                           nonselection_fill_alpha=0.2,
+                           nonselection_fill_color="blue",
+                           nonselection_line_color="firebrick",
+                           nonselection_line_alpha=1.0)
 
-    fig_js, fig_div = components(p)
+    plot.xaxis.axis_label = "x"
+    plot.yaxis.axis_label = "y"
+
+    fig_js, fig_div = components(plot)
     return render_template("figures.html", figJS=fig_js, figDiv=fig_div)
 
 
